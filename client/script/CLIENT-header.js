f25c8224750c40b4e4fca849db51a7d57bac2ab1
BlackE <elizabeth.ramescamilla@gmail.com>
2020-06-30 23:18:25 -0500

    fix(style): styling views

--- client/script/CLIENT-header.js
+++ client/script/CLIENT-header.js
@@ -7,10 +7,13 @@
 		// console.log(active);
 		if(active !== null){
 			active.classList.add('active');
+		}else{
+			document.getElementById('nav_index').classList.add('active');
 		}
 	}else{
 		let liActive = urlActive.split('?');
 		switch(liActive[0]){
+			case 'order': document.getElementById('nav_orders').classList.add('active');break;
 			case 'billing': document.getElementById('nav_billings').classList.add('active');break;
 			case 'shipping': document.getElementById('nav_shippings').classList.add('active');break;
 		}
